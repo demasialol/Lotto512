9151b5e450111e37f13353113214d64e4eeab17d
demasialol <demasialol@naver.com>
2021-05-13 14:48:53 +0900

    RelativeLayout for ConstellationActivity 0513 HW Complete

--- app/src/main/java/com/androidapp/lotto512/MainActivity.kt
+++ app/src/main/java/com/androidapp/lotto512/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.androidapp.lotto512
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.ConstellationCard).setOnClickListener{
+            val intent = Intent(this,ConstellationActivity::class.java)
+            startActivity(intent)
+        }
+
     }
+
+
+
 
 }
